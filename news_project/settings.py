efe095c22040f8aa19a523fc516dcf82365e39e1
kendynweb <kendynweb@gmail.com>
2023-12-03 15:09:02 -0500

    updated database

--- news_project/settings.py
+++ news_project/settings.py
@@ -3,6 +3,9 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 
+import dj_database_url
+
+
 # Load the environment variables from the .env file
 load_dotenv()
 
@@ -84,12 +87,8 @@ WSGI_APPLICATION = 'news_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
