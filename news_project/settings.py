8c389d9f1e4dda7169b01626d37d934e4801fc3a
kendynweb <kendynweb@gmail.com>
2023-12-03 15:31:22 -0500

    updated

--- news_project/settings.py
+++ news_project/settings.py
@@ -88,9 +88,11 @@ WSGI_APPLICATION = 'news_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
