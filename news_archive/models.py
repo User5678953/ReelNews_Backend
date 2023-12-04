4878c60a58d2d9d6a698c28ef55e63ab6c70ae87
kendynweb <kendynweb@gmail.com>
2023-12-04 13:57:09 -0500

    updated user

--- news_archive/models.py
+++ news_archive/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User 
 
 class NewsArchive(models.Model):
     source_id = models.CharField(max_length=100, null=True, blank=True)
