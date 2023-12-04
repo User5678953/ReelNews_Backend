c004338e88e98877db0f18b4db29ee570dc93c90
kendynweb <kendynweb@gmail.com>
2023-12-04 13:56:04 -0500

    updated models

--- news_archive/models.py
+++ news_archive/models.py
@@ -10,6 +10,8 @@ class NewsArchive(models.Model):
     url_to_image = models.URLField(max_length=500, null=True, blank=True)
     published_at = models.DateTimeField()
     content = models.TextField(null=True, blank=True)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True)
+    source = models.CharField(max_length=255, null=True, blank=True)
 
     def __str__(self):
         return self.title
